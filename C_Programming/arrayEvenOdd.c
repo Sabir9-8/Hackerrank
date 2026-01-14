53a31cbbe155ce6bf084bf8f39e61055a5084f65
Md Sabir Ahamed <mdahamedsabir98@gmail.com>
2026-01-14 21:59:16 +0530

    hackerrankPractice

---
+++ C_Programming/arrayEvenOdd.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    
+    for (int i=0; i < n; ++i) {
+        scanf("%d", &arr[i]);
+    }
+    int even_count = 0;
+    int odd_count = 0;
+    
+    for (int i=0; i < n; ++i) {
+        if (arr[i] & 1) {
+            ++odd_count;
+        }
+        else {
+            ++even_count;
+        }
+    }
+    printf("%d %d", even_count, odd_count);
+
+    return 0;
+}
